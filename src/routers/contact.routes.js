38b5a0452d33617991b1de6a7a7508149181998a
Quynh Do <doducquynh14022000@gmail.com>
2026-01-25 22:50:19 +0700

    logo

--- src/routers/contact.routes.js
+++ src/routers/contact.routes.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const blogCategoryController = require('../controllers/blog-category.controller')
 const { authenticate } = require('../middlewares/auth.middleware')
 
-router.get('/', blogCategoryController.getAll)
+router.get('/', authenticate, blogCategoryController.getAll)
 router.get('/:id', blogCategoryController.getById)
-router.post('/', authenticate, blogCategoryController.create)
+router.post('/', blogCategoryController.create)
 router.put('/:id', authenticate, blogCategoryController.update)
 router.delete('/:id', authenticate, blogCategoryController.remove)
 
