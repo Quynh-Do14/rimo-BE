71193b03085001da769e4d64e076782fbd533887
Quynh Do <doducquynh14022000@gmail.com>
2026-02-11 08:17:53 +0700

    env

--- src/models/slogan.model.js
+++ src/models/slogan.model.js
@@ -24,7 +24,7 @@ const getAllSlogan = async ({ page = 1, limit = 10, search = '' }) => {
   // Thêm phân trang
   queryParams.push(limit)
   queryParams.push(offset)
-  query += ` ORDER BY id DESC LIMIT $${queryParams.length - 1} OFFSET $${
+  query += ` ORDER BY id ASC LIMIT $${queryParams.length - 1} OFFSET $${
     queryParams.length
   }`
 
